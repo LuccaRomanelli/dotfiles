7e13100d87dc1b48a30a1268077b7ee04de5c1b6
Lucca Romanelli <ldpromanelli@gmail.com>
2026-01-27 12:33:29 -0300

    todo

--- nvim/.config/nvim/lua/todo.lua
+++ nvim/.config/nvim/lua/todo.lua
@@ -1,15 +1,29 @@
 local M = {}
 
-local todo_path = "/home/lcc/obisidian/todo.md"
+local paths = {
+  personal = "/home/lcc/obisidian/todo.md",
+  work = "/home/lcc/obisidian/todo-work.md",
+  vanta = "/home/lcc/obisidian/todo-vanta.md",
+}
+
+--- Retorna o path baseado no tipo
+--- @param todo_type string|nil Tipo do todo (personal, work, vanta)
+--- @return string
+local function get_path(todo_type)
+  local t = todo_type or "personal"
+  return paths[t] or paths.personal
+end
 
 --- Adiciona uma tarefa ao arquivo todo.md
 --- @param text string Texto da tarefa
-function M.add_task(text)
+--- @param todo_type string|nil Tipo do todo (personal, work, vanta)
+function M.add_task(text, todo_type)
   if not text or text == "" then
     vim.notify("Texto da tarefa não pode ser vazio", vim.log.levels.WARN)
     return
   end
 
+  local todo_path = get_path(todo_type)
   local file_exists = vim.fn.filereadable(todo_path) == 1
   local lines = {}
 
@@ -28,7 +42,10 @@ function M.add_task(text)
 end
 
 --- Abre o arquivo todo.md no Neovim
-function M.open_todo()
+--- @param todo_type string|nil Tipo do todo (personal, work, vanta)
+function M.open_todo(todo_type)
+  local todo_path = get_path(todo_type)
+
   -- Cria arquivo com header se não existir
   if vim.fn.filereadable(todo_path) ~= 1 then
     local dir = vim.fn.fnamemodify(todo_path, ":h")
@@ -40,9 +57,9 @@ function M.open_todo()
 end
 
 -- Comandos Neovim
-vim.api.nvim_create_user_command("Todo", function()
-  M.open_todo()
-end, { desc = "Abre o arquivo todo.md" })
+vim.api.nvim_create_user_command("Todo", function(opts)
+  M.open_todo(opts.args ~= "" and opts.args or nil)
+end, { nargs = "?", desc = "Abre o arquivo todo.md" })
 
 vim.api.nvim_create_user_command("TodoAdd", function(opts)
   M.add_task(opts.args)
